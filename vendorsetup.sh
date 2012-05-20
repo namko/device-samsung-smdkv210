1a4cb4a2ac378f663cd2731bfa10648bcc90c3ca
Navneet Kambo <navneet117@gmail.com>
2012-05-20 18:24:56 +0530

    Added "user" build lunch combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo full_smdkv210-user
 add_lunch_combo full_smdkv210-userdebug
